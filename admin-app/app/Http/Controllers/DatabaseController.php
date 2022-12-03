a0d57e780f6220538a8ad0bd1828346ee60ef867
Andreu Seguí <andreuseguisegura@gmail.com>
2022-12-03 16:13:02 +0100

    Mas consultas y registros/updates/deletes

--- admin-app/app/Http/Controllers/DatabaseController.php
+++ admin-app/app/Http/Controllers/DatabaseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseController extends Controller
 {
@@ -13,17 +14,28 @@ class DatabaseController extends Controller
          * Para empezar a hacer consultas a la base de datos siempre se tiene que poner: DB::
          * * Devolviendo una coleccion, las colccions permiten hacer muchas mas funciones que los arrays.
          * * Por ejemplo podrias pasar la coleccion a json, array...
-        */
+         */
         $users = DB::table('users')
-            ->where('name', '<>', null) /* Listamos todos los nombres que no esten nulos */
+            ->where('name', '<>', null) /** Listamos todos los nombres que no esten nulos */
             ->where('name', 'like', "A%")
             ->whereBetween('email_verified_at', ['2022-10-1', '2022-12-6'])
-            /**
-             * * ->WhereRaw() sirve para poner las consultas directametnte. Ex: SELECT * FROM users WHERE (id = 1 or id = 2)
-             * SQL puro
-             */
             ->get();
         print_r($users);
+
+        $users = DB::table('users')
+            ->selectRaw('users.*, user_roles.label')
+            ->where('users.id', 1)
+            ->join ('user_roles', 'users.role_id', '=', 'user_roles.id')
+            ->get();
+        print_r($users);
+            /**
+             * * ->selectRaw() sirve para poner las consultas directametnte. Ex: selectRaw(users.*, user_roles.level as user_level)
+             * * ->whereRaw() sirve para poner las consultas directametnte. Ex: whereRaw((id = 1 or id = 2))
+             * SQL puro
+             * Ademos tambien se puede hacer un
+             * * $users = DB::table('users)->select(DB::raw())->get();
+            */
+
         /**
          * Asi develve un objeto en vez de una coleccion
          * el foreach pasa los datos a un array
@@ -31,5 +43,18 @@ class DatabaseController extends Controller
          * *    print_r($u);
          * * }
          */
+
+        /**
+         * ->insert() para insertar registros
+         * ->update() para actualizar registros
+         * ->updateorinsert() para actualizar o insertar registros
+         * ->delete() para eliminarlos
+         */
+        $password = Hash::make('password');
+        DB::table('users')->where('id', 11)->update([
+            'name' => 'Andreu',
+            'email' => 'correo@gmail.com',
+            'password' => $password
+        ]);
     }
 }
