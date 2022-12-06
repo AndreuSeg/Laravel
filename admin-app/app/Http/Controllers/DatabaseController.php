96c9757e37048b18903941d6ecc43d9a6f4dd869
Andreu Seguí <andreuseguisegura@gmail.com>
2022-12-07 00:01:44 +0100

    Database controller y modeltable

--- admin-app/app/Http/Controllers/DatabaseController.php
+++ admin-app/app/Http/Controllers/DatabaseController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\ModelTable;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -12,22 +14,22 @@ class DatabaseController extends Controller
     {
         /**
          * Para empezar a hacer consultas a la base de datos siempre se tiene que poner: DB::
-         * * Devolviendo una coleccion, las colccions permiten hacer muchas mas funciones que los arrays.
-         * * Por ejemplo podrias pasar la coleccion a json, array...
+         * Devolviendo una coleccion, las colccions permiten hacer muchas mas funciones que los arrays.
+         * Por ejemplo podrias pasar la coleccion a json, array...
          */
-        $users = DB::table('users')
-            ->where('name', '<>', null) /** Listamos todos los nombres que no esten nulos */
-            ->where('name', 'like', "A%")
-            ->whereBetween('email_verified_at', ['2022-10-1', '2022-12-6'])
-            ->get();
-        print_r($users);
+        // $users = DB::table('users')
+        //     ->where('name', '<>', null) /** Listamos todos los nombres que no esten nulos */
+        //     ->where('name', 'like', "A%")
+        //     ->whereBetween('email_verified_at', ['2022-10-1', '2022-12-6'])
+        //     ->get();
+        // print_r($users);
 
-        $users = DB::table('users')
-            ->selectRaw('users.*, user_roles.label')
-            ->where('users.id', 1)
-            ->join ('user_roles', 'users.role_id', '=', 'user_roles.id')
-            ->get();
-        print_r($users);
+        // $users = DB::table('users')
+        //     ->selectRaw('users.*, user_roles.label')
+        //     ->where('users.id', 1)
+        //     ->join ('user_roles', 'users.role_id', '=', 'user_roles.id')
+        //     ->get();
+        // print_r($users);
             /**
              * * ->selectRaw() sirve para poner las consultas directametnte. Ex: selectRaw(users.*, user_roles.level as user_level)
              * * ->whereRaw() sirve para poner las consultas directametnte. Ex: whereRaw((id = 1 or id = 2))
@@ -45,16 +47,37 @@ class DatabaseController extends Controller
          */
 
         /**
-         * ->insert() para insertar registros
-         * ->update() para actualizar registros
-         * ->updateorinsert() para actualizar o insertar registros
-         * ->delete() para eliminarlos
+         * * ->insert() para insertar registros
+         * * ->update() para actualizar registros
+         * * ->updateorinsert() para actualizar o insertar registros
+         * * ->delete() para eliminarlos
          */
-        $password = Hash::make('password');
-        DB::table('users')->where('id', 11)->update([
-            'name' => 'Andreu',
-            'email' => 'correo@gmail.com',
-            'password' => $password
-        ]);
+        // $password = Hash::make('password');
+        // DB::table('users')->where('id', 11)->update([
+        //     'name' => 'Andreu',
+        //     'email' => 'correo@gmail.com',
+        //     'password' => $password
+        // ]);
+
+
+
+        // Eloquent
+        /**
+         * Eloquent es un ORM. Y trabaja con modelos
+         */
+        // $user = User::find(1);
+        // Esta es una manera de cambiar un campo
+        // $user->name = 'Alfreda';
+        // Esta es otra
+        // $user->fill([
+        //     'name' => 'Alfreda Teruel',
+        //     'email' => 'example@gmail.com'
+        // ]);
+        // Para guardar hay que especificarlo con: save();
+        // $user->save();
+
+        // ModelTable::create([
+        //     'name' => 'Example2'
+        // ]);
     }
 }
